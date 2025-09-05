f06ec15df5b562c6d128f66b28ef0bd973c8e99e
onedreamer <onedreamer0129@gmail.com>
2025-09-05 13:31:23 +0800

    fix: 修复证书生产bug

--- src/main/java/cn/edu/gfkd/evidence/service/CertificateConfig.java
+++ src/main/java/cn/edu/gfkd/evidence/service/CertificateConfig.java
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.Configuration;
 
 import lombok.Data;
 
-@Configuration
-@ConfigurationProperties(prefix = "certificate")
-@Data
+@Configuration @ConfigurationProperties(prefix = "certificate") @Data
 public class CertificateConfig {
-    
+
     private String templatePath = "classpath:static/proof_template.pdf";
-    private String outputPath = "./certificates";
+    private String outputPath = "data/certificates";
     private String fontPath = "classpath:static/fonts/simhei.ttf";
     private boolean autoCreateDirectory = true;
-    
+
 }
